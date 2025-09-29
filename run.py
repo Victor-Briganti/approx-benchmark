7c17b3bcf3d0c664abac051c1b58183ad8ccacc8
Victor Briganti <jvbriganti@gmail.com>
2025-09-29 19:25:57 -0300

    chore/refac-bench: Changed setup phase

--- run.py
+++ run.py
@@ -1,16 +1,11 @@
+import duckdb
 from zipfile import ZipFile
 import os
 import random as rand
+import pandas as pd
+import numpy as np
 
-APPLICATIONS: list[str] = [
-    "2mm",
-    "correlation",
-    "deriche",
-    "jacobi2d",
-    "kmeans",
-    "mandelbrot",
-    "pi",
-]
+DATABASE = duckdb.connect("database.db", read_only=False)
 
 APPLICATION_DIR: str = "applications"  # src of the apps
 OUTPUT_DIR: str = "output"  # outputs of the running apps
@@ -20,20 +15,25 @@ REPORT_DIR: str = "report"  # graphs and analytics
 NUM_EXEC: int = 10
 THREADS: list[int] = [1, 2, 4, 8]
 
+def create_dir(applications):
+    # Create the default directories for all applications
+    for app in applications['name']:
+        for dir in [APPLICATION_DIR, REPORT_DIR]:
+            if dir == APPLICATION_DIR:
+                if not os.path.exists(f"{dir}/{app}/{OUTPUT_DIR}"):
+                    os.makedirs(f"{dir}/{app}/{OUTPUT_DIR}")
+                    
+                if not os.path.exists(f"{dir}/{app}/{PERFORMANCE_DIR}"):
+                    os.makedirs(f"{dir}/{app}/{PERFORMANCE_DIR}")
 
-def create_dir():
-    for app in APPLICATIONS:
-        for dir in [OUTPUT_DIR, PERFORMANCE_DIR, REPORT_DIR]:
-            if not os.path.exists(f"{dir}/{app}"):
-                os.makedirs(f"{dir}/{app}/common")
-                os.makedirs(f"{dir}/{app}/omp")
-                os.makedirs(f"{dir}/{app}/approx")
+            elif dir == REPORT_DIR:
+                if not os.path.exists(f"{dir}/{app}"):
+                    os.makedirs(f"{dir}/{app}/common")
+                    os.makedirs(f"{dir}/{app}/omp")
+                    os.makedirs(f"{dir}/{app}/approx")
 
-                if app == "2mm":
+                if app == "2mm" and not os.path.exists(f"{dir}/{app}/base"):
                     os.makedirs(f"{dir}/{app}/base")
-
-    if not os.path.exists(f"{APPLICATION_DIR}/correlation/input"):
-        os.makedirs(f"{APPLICATION_DIR}/correlation/input")
 
 
 def unzip_file(input_path: str, output_path: str):
@@ -41,36 +41,21 @@ def unzip_file(input_path: str, output_path: str):
         zip.extractall(output_path)
 
 
-def generate_csv(rows: int, columns: int, output_path: str):
-    rand.seed(0)
-    with open(f"{output_path}", "w") as file:
-        _ = file.write(f"{columns} {rows}\n")
-
-        for _ in range(rows):
-            for i in range(0, columns):
-                _ = file.write(str(rand.random()))
-
-                if i + 1 == columns:
-                    _ = file.write("\n")
-                else:
-                    _ = file.write(",")
-
-
-def setup():
-    create_dir()
+def setup(applications):
+    create_dir(applications)
     unzip_file(
-        f"{APPLICATION_DIR}/kmeans/input/kdd_cup.csv.zip",
+        f"{APPLICATION_DIR}/correlation/input/input.zip",
+        f"{APPLICATION_DIR}/correlation/input/",
+    )
+    unzip_file(
+        f"{APPLICATION_DIR}/kmeans/input/kdd_cup.zip",
         f"{APPLICATION_DIR}/kmeans/input/",
     )
-    generate_csv(8192, 256, f"{APPLICATION_DIR}/correlation/input/input.csv")
-
-
-
-
-#def run():
-
-
 
 
 if __name__ == "__main__":
-    setup()
+    applications = DATABASE.execute("""
+        SELECT DISTINCT name FROM benchmark WHERE canceled = false;
+    """).df()
+    
+    setup(applications)
