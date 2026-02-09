5728ebf69768fe7178622580f7108ef89bbab344
Victor Briganti <jvbriganti@gmail.com>
2026-02-09 20:30:00 -0300

    feat/alter-apps: Make changes on deriche

--- applications/deriche/deriche.cpp
+++ applications/deriche/deriche.cpp
@@ -121,84 +121,133 @@ unsigned char *convert(const float *image, int width, int height) {
 float *deriche(const float *imageIn, int width, int height, float alpha) {
   float *imageOut = new float[width * height]();
 
-  const float k = (1.0f - expf(-alpha)) * (1.0f - expf(-alpha)) /
-                  (1.0f + 2.0f * alpha * expf(-alpha) - expf(-2.0f * alpha));
-  const float a1 = k;
-  const float a2 = k * expf(-alpha) * (alpha - 1.0f);
-  const float a3 = k * expf(-alpha) * (alpha + 1.0f);
-  const float a4 = -k * expf(-2.0f * alpha);
-  const float a5 = a1;
-  const float a6 = a2;
-  const float a7 = a3;
-  const float a8 = a4;
-  const float b1 = powf(2.0f, -alpha);
-  const float b2 = -expf(-2.0f * alpha);
-
-#pragma omp parallel shared(imageOut) num_threads(NUM_THREADS)
+#ifdef FASTMATH
+#pragma omp approx fastmath
+#endif
   {
+    const float k = (1.0f - expf(-alpha)) * (1.0f - expf(-alpha)) /
+                    (1.0f + 2.0f * alpha * expf(-alpha) - expf(-2.0f * alpha));
+    const float a1 = k;
+    const float a2 = k * expf(-alpha) * (alpha - 1.0f);
+    const float a3 = k * expf(-alpha) * (alpha + 1.0f);
+    const float a4 = -k * expf(-2.0f * alpha);
+    const float a5 = a1;
+    const float a6 = a2;
+    const float a7 = a3;
+    const float a8 = a4;
+    const float b1 = powf(2.0f, -alpha);
+    const float b2 = -expf(-2.0f * alpha);
+#pragma omp parallel shared(imageOut) num_threads(NUM_THREADS)
+    {
+#ifdef PERFO_LARGE
+#pragma omp for approx perfo(large, DROP) schedule(static)
+#endif
+#ifdef PERFO_INIT
+#pragma omp for approx perfo(init, DROP) schedule(static)
+#endif
+#ifdef PERFO_FINI
+#pragma omp for approx perfo(fini, DROP) schedule(static)
+#endif
+#ifdef OMP
 #pragma omp for schedule(static)
-    for (int i = 0; i < width; i++) {
-      float xm1 = 0.0f;
-      float ym1 = 0.0f;
-      float ym2 = 0.0f;
-      for (int j = 0; j < height; j++) {
-        imageOut[j * width + i] =
-            a1 * imageIn[j * width + i] + a2 * xm1 + b1 * ym1 + b2 * ym2;
-        xm1 = imageIn[j * width + i];
-        ym2 = ym1;
-        ym1 = imageOut[j * width + i];
-      }
-    }
-
-#pragma omp for schedule(static)
-    for (int i = 0; i < width; i++) {
-      float xp1 = 0.0f;
-      float xp2 = 0.0f;
-      float yp1 = 0.0f;
-      float yp2 = 0.0f;
-      for (int j = height - 1; j >= 0; j--) {
-        const float prev = imageOut[j * width + i];
-        const float res = a3 * xp1 + a4 * xp2 + b1 * yp1 + b2 * yp2;
-        imageOut[j * width + i] += res;
-        xp2 = xp1;
-        xp1 = res;
-        yp2 = yp1;
-        yp1 = prev;
-      }
-    }
-
-#pragma omp for schedule(static)
-    for (int j = 0; j < height; j++) {
-      float tm1 = 0.0f;
-      float ym1 = 0.0f;
-      float ym2 = 0.0f;
+#endif
       for (int i = 0; i < width; i++) {
-        const float prev = imageOut[j * width + i];
-        const float res =
-            a5 * imageOut[j * width + i] + a6 * tm1 + b1 * ym1 + b2 * ym2;
-        imageOut[j * width + i] += res;
-        tm1 = prev;
-        ym2 = ym1;
-        ym1 = res;
+        float xm1 = 0.0f;
+        float ym1 = 0.0f;
+        float ym2 = 0.0f;
+        for (int j = 0; j < height; j++) {
+          imageOut[j * width + i] =
+              a1 * imageIn[j * width + i] + a2 * xm1 + b1 * ym1 + b2 * ym2;
+          xm1 = imageIn[j * width + i];
+          ym2 = ym1;
+          ym1 = imageOut[j * width + i];
+        }
+      }
+
+#ifdef PERFO_LARGE
+#pragma omp for approx perfo(large, DROP) schedule(static)
+#endif
+#ifdef PERFO_INIT
+#pragma omp for approx perfo(init, DROP) schedule(static)
+#endif
+#ifdef PERFO_FINI
+#pragma omp for approx perfo(fini, DROP) schedule(static)
+#endif
+#ifdef OMP
+#pragma omp for schedule(static)
+#endif
+      for (int i = 0; i < width; i++) {
+        float xp1 = 0.0f;
+        float xp2 = 0.0f;
+        float yp1 = 0.0f;
+        float yp2 = 0.0f;
+        for (int j = height - 1; j >= 0; j--) {
+          const float prev = imageOut[j * width + i];
+          const float res = a3 * xp1 + a4 * xp2 + b1 * yp1 + b2 * yp2;
+          imageOut[j * width + i] += res;
+          xp2 = xp1;
+          xp1 = res;
+          yp2 = yp1;
+          yp1 = prev;
+        }
+      }
+
+#ifdef PERFO_LARGE
+#pragma omp for approx perfo(large, DROP) schedule(static)
+#endif
+#ifdef PERFO_INIT
+#pragma omp for approx perfo(init, DROP) schedule(static)
+#endif
+#ifdef PERFO_FINI
+#pragma omp for approx perfo(fini, DROP) schedule(static)
+#endif
+#ifdef OMP
+#pragma omp for schedule(static)
+#endif
+      for (int j = 0; j < height; j++) {
+        float tm1 = 0.0f;
+        float ym1 = 0.0f;
+        float ym2 = 0.0f;
+        for (int i = 0; i < width; i++) {
+          const float prev = imageOut[j * width + i];
+          const float res =
+              a5 * imageOut[j * width + i] + a6 * tm1 + b1 * ym1 + b2 * ym2;
+          imageOut[j * width + i] += res;
+          tm1 = prev;
+          ym2 = ym1;
+          ym1 = res;
+        }
+      }
+
+#ifdef PERFO_LARGE
+#pragma omp for approx perfo(large, DROP) schedule(static)
+#endif
+#ifdef PERFO_INIT
+#pragma omp for approx perfo(init, DROP) schedule(static)
+#endif
+#ifdef PERFO_FINI
+#pragma omp for approx perfo(fini, DROP) schedule(static)
+#endif
+#ifdef OMP
+#pragma omp for schedule(static)
+#endif
+      for (int j = 0; j < height; j++) {
+        float tp1 = 0.0f;
+        float tp2 = 0.0f;
+        float yp1 = 0.0f;
+        float yp2 = 0.0f;
+        for (int i = width - 1; i >= 0; i--) {
+          const float prev = imageOut[j * width + i];
+          const float res = a7 * tp1 + a8 * tp2 + b1 * yp1 + b2 * yp2;
+          imageOut[j * width + i] += res;
+          tp2 = tp1;
+          tp1 = prev;
+          yp2 = yp1;
+          yp1 = res;
+        }
       }
     }
 
-#pragma omp for schedule(static)
-    for (int j = 0; j < height; j++) {
-      float tp1 = 0.0f;
-      float tp2 = 0.0f;
-      float yp1 = 0.0f;
-      float yp2 = 0.0f;
-      for (int i = width - 1; i >= 0; i--) {
-        const float prev = imageOut[j * width + i];
-        const float res = a7 * tp1 + a8 * tp2 + b1 * yp1 + b2 * yp2;
-        imageOut[j * width + i] += res;
-        tp2 = tp1;
-        tp1 = prev;
-        yp2 = yp1;
-        yp1 = res;
-      }
-    }
   }
 
   return imageOut;
